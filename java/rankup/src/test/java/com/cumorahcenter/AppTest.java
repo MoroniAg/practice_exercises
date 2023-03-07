7fea417af7b142d91c58f739071512151cd195c0
Moroni Aguilera <moroniaguilera1993@gmail.com>
2023-03-07 12:33:31 -0500

    rm3s4 - add exercise for extract range of list of numbers

--- java/rankup/src/test/java/com/cumorahcenter/AppTest.java
+++ java/rankup/src/test/java/com/cumorahcenter/AppTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import com.cumorahcenter.month_02.week_05.PigLatin;
 import com.cumorahcenter.month_02.week_05.RgbToHex;
+import com.cumorahcenter.month_03.week_01.RangeExtraction;
 import com.cumorahcenter.month_03.week_01.TopWords;
 import org.junit.Test;
 
@@ -51,7 +52,7 @@ public class AppTest {
 		assertEquals(List.of("e") , TopWords.top3("  , e   .. "));
 		assertEquals(new ArrayList<>() , TopWords.top3("  ...  "));
 		assertEquals(new ArrayList<>() , TopWords.top3("  '  "));
-		assertEquals(new ArrayList<>(), TopWords.top3("  '''  "));
+		assertEquals(new ArrayList<>() , TopWords.top3("  '''  "));
 		StringJoiner joiner = new StringJoiner("\n");
 		for (String s : Arrays.asList(
 				"In a village of La Mancha, the name of which I have no desire to call to" ,
@@ -62,7 +63,43 @@ public class AppTest {
 				"on Sundays, made away with three-quarters of his income.")) {
 			joiner.add(s);
 		}
-		assertEquals(asList("a" , "of" , "on") ,
-					 TopWords.top3(joiner.toString()));
+		assertEquals(asList("a" , "of" , "on") , TopWords.top3(joiner.toString()));
 	}
+	
+	@Test
+	public void test_BasicTests() {
+		assertEquals("-6,-3-1,3-5,7-11,14,15,17-20" ,
+					 RangeExtraction.rangeExtraction(new int[]{-6 ,
+							 -3 ,
+							 -2 ,
+							 -1 ,
+							 0 ,
+							 1 ,
+							 3 ,
+							 4 ,
+							 5 ,
+							 7 ,
+							 8 ,
+							 9 ,
+							 10 ,
+							 11 ,
+							 14 ,
+							 15 ,
+							 17 ,
+							 18 ,
+							 19 ,
+							 20}));
+		assertEquals("-3--1,2,10,15,16,18-20" ,
+					 RangeExtraction.rangeExtraction(new int[]{-3 ,
+							 -2 ,
+							 -1 ,
+							 2 ,
+							 10 ,
+							 15 ,
+							 16 ,
+							 18 ,
+							 19 ,
+							 20}));
+	}
+	
 }
